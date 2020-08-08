c152a82f2fc8c80b705b710d3e4a530016f0aca5
VinayParmarJain <Vinayparmarjain@hotmail.com>
2020-08-09 03:04:47 +0530

    Updates

---
+++ precache-manifest.bb1657f62645bafe319fc40206b285e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcfb340b0b6fd7929e33c597ca0fae6",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "167134a4c024951deb08",
+    "url": "/instagram/static/css/main.56caae78.chunk.css"
+  },
+  {
+    "revision": "0c686b393ca742fd37de",
+    "url": "/instagram/static/js/2.ee0b6090.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram/static/js/2.ee0b6090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167134a4c024951deb08",
+    "url": "/instagram/static/js/main.1a493740.chunk.js"
+  },
+  {
+    "revision": "060ad16ba7f5db70f5a5",
+    "url": "/instagram/static/js/runtime-main.a87bf05b.js"
+  }
+]);
