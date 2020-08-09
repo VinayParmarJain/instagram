967ea2bc5cc01e91168d9478f042d2146067cd06
VinayParmarJain <Vinayparmarjain@hotmail.com>
2020-08-09 05:50:23 +0530

    Updates

---
+++ precache-manifest.109be4f3cb1b96aed8d33a8a05b0fbc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678cc9ddfacd311338abcbeedb8e7f32",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "fde0ab49c0f2066f2580",
+    "url": "/instagram/static/css/main.56caae78.chunk.css"
+  },
+  {
+    "revision": "c1d79aeef37caccb99b7",
+    "url": "/instagram/static/js/2.335127fb.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram/static/js/2.335127fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde0ab49c0f2066f2580",
+    "url": "/instagram/static/js/main.0a282910.chunk.js"
+  },
+  {
+    "revision": "060ad16ba7f5db70f5a5",
+    "url": "/instagram/static/js/runtime-main.a87bf05b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/instagram/precache-manifest.bb1657f62645bafe319fc40206b285e2.js"
+  "/instagram/precache-manifest.109be4f3cb1b96aed8d33a8a05b0fbc5.js"
 );
 
 self.addEventListener('message', (event) => {
